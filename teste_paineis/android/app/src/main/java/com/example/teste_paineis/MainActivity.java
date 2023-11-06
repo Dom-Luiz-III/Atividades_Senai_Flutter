0bb750da90222fabbe32b591f4d0e99c64ebc082
Luiz Henrique Carneiro Carvalho <111710021+Dom-Luiz-III@users.noreply.github.com>
2023-11-06 11:23:05 -0300

    teste segunda página

---
+++ teste_paineis/android/app/src/main/java/com/example/teste_paineis/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.teste_paineis;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ teste_paineis/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'segunda_pagina.dart';
+
+
+void main() {
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Teste Arquivos separados',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+      routes: {
+        '/segunda': (context) => SegundaPage(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Teste Arquivos separados'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Ir para a Segunda Página'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/segunda');
+          },
+        ),
+      ),
+    );
+  }
+}
