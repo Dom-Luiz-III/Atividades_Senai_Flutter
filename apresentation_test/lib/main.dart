939b17d23decbd2f3213011555244a0e2d27e213
Luiz Henrique Carneiro Carvalho <111710021+Dom-Luiz-III@users.noreply.github.com>
2023-12-11 11:12:15 -0300

    Update main.dart

--- apresentation_test/lib/main.dart
+++ apresentation_test/lib/main.dart
@@ -26,58 +26,88 @@ class GoogleBottomBar extends StatefulWidget {
 
 class _GoogleBottomBarState extends State<GoogleBottomBar> {
   int _selectedIndex = 0;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: const Color(0xFF22272B),
       appBar: AppBar(
-          title: const Text(
-            'AGENCLEAN contratante',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: const Color(0xFF22272B)),
+        title: const Text(
+          'AGENCLEAN contratante',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color(0xFF22272B),
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () => _scaffoldKey.currentState?.openDrawer(),
+        ),
+      ),
+      drawer: _drawer(),
       body: Center(
         child: _navBarItems[_selectedIndex].title,
       ),
       bottomNavigationBar: SalomonBottomBar(
-          currentIndex: _selectedIndex,
-          selectedItemColor: const Color(0xff6200ee),
-          unselectedItemColor: const Color.fromARGB(255, 255, 255, 255),
-          onTap: (index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
-          items: _navBarItems,
-          backgroundColor: Colors.orange[900]),
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xff6200ee),
+        unselectedItemColor: const Color.fromARGB(255, 255, 255, 255),
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: _navBarItems,
+        backgroundColor: Colors.orange[900],
+      ),
     );
   }
-}
 
-final _navBarItems = [
-  SalomonBottomBarItem(
-    icon: const Icon(Icons.home),
-    title: const Text("Home"),
-    selectedColor: Colors.blue[50],
-  ),
-  SalomonBottomBarItem(
-    icon: const Icon(Icons.search),
-    title: const Text("Search"),
-    selectedColor: Colors.blue[50],
-  ),
-  SalomonBottomBarItem(
-    icon: const Icon(Icons.add_circle_outlined),
-    title: const Text("Likes"),
-    selectedColor: Colors.blue[50],
-  ),
-  SalomonBottomBarItem(
-    icon: const Icon(Icons.chat),
-    title: const Text("Chat"),
-    selectedColor: Colors.blue,
-  ),
-  SalomonBottomBarItem(
-    icon: const Icon(Icons.person),
-    title: const Text("Profile"),
-    selectedColor: Colors.blue[50],
-  ),
-];
+  Widget _drawer() => Drawer(
+        child: ListView(
+          children: _menuItems
+              .map((item) => ListTile(
+                    onTap: () {
+                      _scaffoldKey.currentState?.openEndDrawer();
+                    },
+                    title: Text(item),
+                  ))
+              .toList(),
+        ),
+      );
+
+  final _navBarItems = [
+    SalomonBottomBarItem(
+      icon: const Icon(Icons.home),
+      title: const Text("Home"),
+      selectedColor: Colors.blue[50],
+    ),
+    SalomonBottomBarItem(
+      icon: const Icon(Icons.search),
+      title: const Text("Search"),
+      selectedColor: Colors.blue[50],
+    ),
+    SalomonBottomBarItem(
+      icon: const Icon(Icons.add_circle_outlined),
+      title: const Text("Likes"),
+      selectedColor: Colors.blue[50],
+    ),
+    SalomonBottomBarItem(
+      icon: const Icon(Icons.chat),
+      title: const Text("Chat"),
+      selectedColor: Colors.blue,
+    ),
+    SalomonBottomBarItem(
+      icon: const Icon(Icons.person),
+      title: const Text("Profile"),
+      selectedColor: Colors.blue[50],
+    ),
+  ];
+
+  final List<String> _menuItems = <String>[
+    'About',
+    'Contact',
+    'Settings',
+    'Sign Out',
+  ];
+}
